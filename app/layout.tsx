1cf56e7b09cdf2a4226401b8d175f7f29ae1c4df
Xiao <69733615+Underwater008@users.noreply.github.com>
2026-02-20 03:01:41 -0800

    Update page name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "JUNK Global",
+  title: "JUNK World",
   description: "A global showcase of university design projects",
 };
 
